d89fb7584f56bf89e0d190c3f780c4df34fc11e9
Robert Rainey <r.anthony.rainey@gmail.com>
2024-02-07 15:32:08 -0600

    Added loop for guessing number until q or correct number is entered

--- guess_number.py
+++ guess_number.py
@@ -1,9 +1,13 @@
 number = 10
 
 print("I'm thinking of a number...")
-guess = int(input("What number am I thinking of? "))
 
-if guess == number:
+guess = input("What number am I thinking of? (q to quit) ")
+while guess != str(number) and guess != 'q':
+   print("Sorry! Wrong number. Try again.")
+   guess = input("What number am I thinking of? (q to quit) ")
+
+if guess == str(number):
    print("Congratulations! You guessed the right number.")
 else:
    print(f"Sorry! The number was {number}.")
