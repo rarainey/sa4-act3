a331be84f714256f39ea3450a4f930f976169d32
Robert Rainey <r.anthony.rainey@gmail.com>
2024-02-07 15:37:17 -0600

    Added hints for if guess is too high or too low (and checks if guess is an integer)

--- guess_number.py
+++ guess_number.py
@@ -4,7 +4,14 @@ print("I'm thinking of a number...")
 
 guess = input("What number am I thinking of? (q to quit) ")
 while guess != str(number) and guess != 'q':
-   print("Sorry! Wrong number. Try again.")
+   try:
+      guess = int(guess)
+      if guess > number:
+         print("Too high. Try again!")
+      else:
+         print("Too low. Try again!")
+   except:
+      print("Guess must be a number!")
    guess = input("What number am I thinking of? (q to quit) ")
 
 if guess == str(number):
