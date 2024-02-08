967d1eaf245fa7e3928e4aecedf34e5b6b30dd90
Robert Rainey <r.anthony.rainey@gmail.com>
2024-02-07 22:25:05 -0600

    Reformatted game to only accept a certain amount of guesses before the user fails

--- guess_number.py
+++ guess_number.py
@@ -1,13 +1,20 @@
 number = 10
+num_guesses = 10
 
 print("I'm thinking of a number...")
 
+print(f"You have {num_guesses} guesses to find it!")
 guess = input("What number am I thinking of? (q to quit) ")
-while guess != str(number) and guess != 'q':
-   print("Sorry! Wrong number. Try again.")
+num_guesses -= 1
+while guess != str(number) and guess != 'q' and num_guesses != 0:
+   print(f"Sorry! Wrong number. Try again. You have {num_guesses} guess(es) left.")
    guess = input("What number am I thinking of? (q to quit) ")
+   num_guesses -= 1
+
 
 if guess == str(number):
    print("Congratulations! You guessed the right number.")
-else:
+elif guess == 'q':
    print(f"Sorry! The number was {number}.")
+elif num_guesses == 0:
+   print(f"Sorry! You ran out of guesses! The number was {number}")
